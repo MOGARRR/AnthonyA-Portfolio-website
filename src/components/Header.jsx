24ce918f1d41098ecc392dcf94d7254565b4f542
AnthonyA <alicea.9a@gmail.com>
2025-06-09 18:04:15 -0600

    Feat: Add basic styling for Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
+
 const Header = () => {
   return (
-    <div>
-      <h1>Anthony Alicea</h1>
-      <div>
-        <span>Projects</span>
-        <span>Education</span>
-        <span>Contact</span>
-        <span>Resume</span>
+    <div className="bg-gradient-to-r from-violet-950 from-20% to-blue-800 to-100% text-stone-50 font-sans p-3 absolute top-0 left-0 w-screen flex justify-between">
+      <h1 className=" text-4xl ">Anthony Alicea</h1>
+      <div className=" text-2xl flex justify-around items-end w-8/12">
+        <span className="hover:text-stone-300 cursor-pointer hover:text-3xl">Projects</span>
+        <span className="hover:text-stone-300 cursor-pointer hover:text-3xl">Education</span>
+        <span className="hover:text-stone-300 cursor-pointer hover:text-3xl">Contact</span>
+        <span className="hover:text-stone-300 cursor-pointer hover:text-3xl">Resume</span>
       </div>
       
     </div>
