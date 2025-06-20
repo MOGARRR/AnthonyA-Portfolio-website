b4dc8449e500c951106b77f555d3af14d1e50d04
AnthonyA <alicea.9a@gmail.com>
2025-06-20 16:48:04 -0600

    Refactor: Change skills to grid layout for a cleaner spacing/border

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,13 +5,13 @@ const Skills = () => {
   return (
     <div className="mt-16">
       <h1 className="text-4xl mb-4">Skills</h1>
-      <div className="border-2 border-stone-950 flex flex-wrap justify-center ">
+      <div className="border border-stone-950 grid grid-cols-5 grid-rows-3 gap-0">
         {skills.map((skill) => (
           <div
             key={skill.id}
-            className="bg-gray-400 border-2  border-stone-950 size-52 flex flex-col justify-around"
+            className="bg-gray-400 border-2  border-stone-950 h-52 flex flex-col justify-around"
           >
-            <div className=" size-28 self-center p-2 ">
+            <div className=" size-32 self-center p-2 ">
               <img className="size-full" src={skill.logo} alt="" />
             </div>
             <h2 className="text-3xl">{skill.name}</h2>
