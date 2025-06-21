c26e7351306bad1665b4ab66d8e31bbfdc660e8e
AnthonyA <alicea.9a@gmail.com>
2025-06-21 14:16:52 -0600

    Refactor: Adjust navbar to be fixed and centered at the top of the screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Header = () => {
   return (
-      <div className=" w-3/5 h-16 flex justify-between items-center bg-gray-100 absolute top-4 rounded-full text-3xl text-stone-50 border border-transparent hover:border-gray-950">
+      <div className=" w-2/3 h-16 flex justify-between items-center bg-gray-100 fixed top-4 left-80 rounded-full text-3xl text-stone-50 border border-transparent hover:border-gray-950">
         <h1 className="ml-5 ">Anthony Alicea</h1>
-        <div className="flex justify-between w-3/5 mr-5">
+        <div className="flex justify-around w-3/5 mr-5">
         <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">About</span>
         <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">Projects</span>
         <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">Contacts</span>
