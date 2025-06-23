cd6c6f35aeb6ad09cb534d635730d1b2d4e15cf6
AnthonyA <alicea.9a@gmail.com>
2025-06-23 14:28:12 -0600

    Feat: Add styles folder for extra css styling and add glowing hover class to contact icons

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import '../styles/Contacts.css'
 
 const Contacts = () => {
   return (
     <div>
       <div dir="rtl" className="bg-gray-600 flex flex-col justify-around  w-33 p-2 fixed left-0 top-1/3 rounded-s-lg border-2 border-stone-950 h-2/6">
-        <div className="m-2 p-1 rounded-full hover:cursor-pointer hover:text-orange-600 "><i class="fa-brands fa-github fa-2xl"></i></div>
-        <div className="m-2 p-1 hover:cursor-pointer hover:text-blue-400"><i class="fa-brands fa-linkedin fa-2xl"></i></div>
-        <div className="m-2 p-1 hover:cursor-pointer hover:text-yellow-600"><i class="fa-solid fa-envelope fa-2xl"></i></div>
-        <div className="m-2 p-1 hover:cursor-pointer hover:text-green-400"><i class="fa-solid fa-phone fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow-git hover:cursor-pointer"><i class="fa-brands fa-github fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow-linke hover:cursor-pointer"><i class="fa-brands fa-linkedin fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow-email hover:cursor-pointer "><i class="fa-solid fa-envelope fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow-phone hover:cursor-pointer "><i class="fa-solid fa-phone fa-2xl"></i></div>
       </div>
     </div>
   )
