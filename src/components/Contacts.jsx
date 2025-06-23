20ac4793877c4d1bd61d33bff3271bf226bc50e9
AnthonyA <alicea.9a@gmail.com>
2025-06-23 15:54:47 -0600

    Feat/Refactor: Refactor/add glow text shadow and add responsive jump to navbar and contact items

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,10 +5,10 @@ const Contacts = () => {
   return (
     <div>
       <div dir="rtl" className="bg-gray-600 flex flex-col justify-around  w-33 p-2 fixed left-0 top-1/3 rounded-s-lg border-2 border-stone-950 h-2/6">
-        <div className="m-2 p-1 rounded-full glow-git hover:cursor-pointer"><i class="fa-brands fa-github fa-2xl"></i></div>
-        <div className="m-2 p-1 rounded-full glow-linke hover:cursor-pointer"><i class="fa-brands fa-linkedin fa-2xl"></i></div>
-        <div className="m-2 p-1 rounded-full glow-email hover:cursor-pointer "><i class="fa-solid fa-envelope fa-2xl"></i></div>
-        <div className="m-2 p-1 rounded-full glow-phone hover:cursor-pointer "><i class="fa-solid fa-phone fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow hover:cursor-pointer"><i class="fa-brands fa-github fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow hover:cursor-pointer"><i class="fa-brands fa-linkedin fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow hover:cursor-pointer "><i class="fa-solid fa-envelope fa-2xl"></i></div>
+        <div className="m-2 p-1 rounded-full glow hover:cursor-pointer "><i class="fa-solid fa-phone fa-2xl"></i></div>
       </div>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import '../styles/Header.css'
 
 
 const Header = () => {
   return (
-      <div className=" w-2/3 h-16 flex justify-between items-center bg-gray-100 fixed top-4 left-80 rounded-full text-3xl text-stone-50 border border-transparent hover:border-gray-950">
-        <h1 className="ml-5 ">Anthony Alicea</h1>
-        <div className="flex justify-around w-3/5 mr-5">
-        <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">About</span>
-        <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">Projects</span>
-        <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">Contacts</span>
-        <span className="border-b-2 border-transparent hover:border-gray-950 hover:cursor-pointer ">Resume</span>
+      <div className="nav flex justify-between items-center fixed top-0 left-0 w-screen h-24 p-1 text-4xl">
+
+        <h1 className="ml-5">Anthony Alicea</h1>
+        <div className="flex justify-evenly w-3/5">
+        <span className="nav-item border-b-2 border-transparent  hover:cursor-pointer hover:self-start ">About</span>
+        <span className="nav-item border-b-2 border-transparent  hover:cursor-pointer hover: ">Projects</span>
+        <span className="nav-item border-b-2 border-transparent  hover:cursor-pointer hover: ">Contacts</span>
+        <span className="nav-item border-b-2 border-transparent  hover:cursor-pointer hover: ">Resume</span>
         </div>
         
       </div> 
