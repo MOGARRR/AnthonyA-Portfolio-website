60309234b2e19db9cc938013b2402facdb8fd921
AnthonyA <alicea.9a@gmail.com>
2025-06-30 11:09:53 -0600

    Refactor: Update about me section and add some styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import "../styles/About.css";
 
 const About = () => {
   return (
-    <div className=" mt-20 text-stone-50">
-      <h3 className="text-4xl"> About me </h3>
+    <div className=" mt-20 f text-stone-50">
+      <h3 className="about text-4xl bolden"> About me </h3>
       <p className="text-2xl">
-        I am a full-stack developer and Lighthouse Labs graduate with hands-on
-        experience in building secure, performant web applications. Skilled in
-        JavaScript, Node.js, and SQL, with a background in server management and
-        front-end design. Known for fast learning, adaptability, and building
-        clean, maintainable code in collaborative environments.
+        I'm a <span className="bolden">full-stack web developer</span> who
+        recently graduated from <span className="bolden">Lighthouse Labs</span>{" "}
+        and looking for an opportunity to be a part of the ever-evolving world
+        of software development! Before tech, I was a{" "}
+        <span className="bolden">professional cook</span> {" "}
+        which helped build a foundation of skills like working in or leading a
+        team, time management, effective communication, and attention to detail
+        that have worked as a foundation for my journey into software
+        development. Some things I like to do after consulting my{" "}
+        <span className="bolden">rubber duck</span> {" "}
+        for the day include playing music, drawing/painting, playing games,
+        reading and writing, and of course cooking!{" "}
+        <span className="bolden">Self-improvement</span> is an important value
+        to me so I always love improving or learning something new, currently
+        I'm learning to write in <span className="bolden">typescript</span> and
+        how to play the {""}
+        <span className="bolden">ocarina</span>! I hope this gave you some
+        insight into who I am and if you'd like to learn more or talk about
+        anything, you can reach out to me, and I'd love to chat!
       </p>
-
-      
     </div>
   );
 };
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import '../styles/ProjectCard.css'
 
 const ProjectCard = (props) => {
   const { title, description, stack, github, image } = props;
+  
 
   return (
     <div className="m-2">
-      <h1 className="border-b-2 border-stone-950 text-4xl">{title}</h1>
+      <h1 className="border-b-2 border-stone-950 text-5xl">{title}</h1>
       <div className="flex">
-        <img className="w-6/12 p-2  border-r-2 border-stone-950" src={image} alt="Front Page of project" />
-        <div className=" mt-4">
-          <span className="bg-gray-200 rounded-full p-3 "><a href={github}><i class="fa-brands fa-github fa-2xl"></i></a></span>
+        <img className="project-img w-6/12 p-2 border-r-2 border-stone-950" src={image} alt="Front Page of project" />
+        <div className="mt-4 ">
+          <span className="bg-gray-200 rounded-full p-3"><a href={github}><i class="fa-brands fa-github fa-2xl"></i></a></span>
           {stack.map((item) => (
-            <span className= " bg-gray-200 rounded-full m-2 p-2 text-xl" key={item.id}>{item.name}</span>
+            <span className= "bg-gray-200 rounded-full m-2 p-2 text-xl" key={item.id}>{item.name}</span>
           ))}
-          <p className="text-2xl mt-6 p-2">{description}</p>
+          <p className="text-3xl mt-6 p-2">{description}</p>
         </div>
       </div>
     </div>
