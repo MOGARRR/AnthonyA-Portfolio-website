e521795105f5a4fa1ec60dc461f371b142e77502
AnthonyA <alicea.9a@gmail.com>
2025-07-16 14:40:50 -0600

    WIP: Add simple HeaderMobile component for layout

---
+++ src/components/HeaderMobile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MobileHeader = () => {
+  return (
+    <div className="bg-red-500 flex justify-between items-center p-3">
+    <h1 className="text-xl ">Anthony Alicea</h1>
+    <i class="fa-solid fa-bars fa-2xl bg-"></i>  
+    </div>
+  )
+};
+
+export default MobileHeader;
